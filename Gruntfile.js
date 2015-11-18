b0543a4d136508878fb97426d1f35443104c5c1e
Sandro Tonon <tonon.san@gmail.com>
2015-11-18 01:13:59 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: '.'
+                }
+            }
+        },
+
+        uglify: {
+            scripts: {
+                expand: true,
+                cwd: 'js/custom/',
+                src: '**.js',
+                dest: 'js/custom/',
+                ext: '.min.js'
+            }
+        },
+
+        less: {
+            styles: {
+                options: {
+                    paths: ['css/styles/']
+                },
+                files: {
+                    'css/styles.css': 'less/styles.less'
+                }
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css/dist',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: 'js/custom/**.js',
+                task: 'jshint:scripts'
+            },
+
+            styles: {
+                files: 'less/**.less',
+                tasks: ['less', 'cssmin']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['less', 'cssmin', 'connect:server', 'watch']);
+    grunt.registerTask('build', ['uglify', 'less', 'cssmin']);
+
+};
